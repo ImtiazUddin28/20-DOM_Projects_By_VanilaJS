7dd0fa1050d665a1f6b5bca597befb2e32304a99
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-08-14 09:25:38 +0600

    first two projects are intialized

---
+++ Counter/script.js
@@ -0,0 +1,20 @@
+// select dom elements
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+
+
+// initial state
+let count = 0;
+let count2 = 0;
+
+// event listeners
+incrementEl.addEventListener("click", () => {
+    count++;
+    counterEl.innerText = count;
+});
+
+decrementEl.addEventListener("click", () => {
+    count--;
+    counterEl.innerText = count;
+});
